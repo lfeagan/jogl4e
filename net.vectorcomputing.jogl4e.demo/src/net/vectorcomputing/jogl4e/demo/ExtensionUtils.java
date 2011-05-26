d2f7a52c1301779d138f4359f625185b8ab0a6e5
Lance Feagan <lfeagan@hotmail.com>
2011-05-26 17:04:23 -0500

    Changed to using open variant of StringUtils which is called StringFormat.

--- net.vectorcomputing.jogl4e.demo/src/net/vectorcomputing/jogl4e/demo/ExtensionUtils.java
+++ net.vectorcomputing.jogl4e.demo/src/net/vectorcomputing/jogl4e/demo/ExtensionUtils.java
@@ -2,13 +2,13 @@ package net.vectorcomputing.jogl4e.demo;
 
 import javax.media.opengl.GL2;
 
-import com.ibm.arcs.basic.strings.StringUtils;
+import net.vectorcomputing.base.string.StringFormat;
 
 public class ExtensionUtils {
 
 	public static final String[] getExtensions(GL2 gl) {
 		final String glExtensionsString = gl.getContext().getGLExtensionsString();
-		String[] extensions = StringUtils.splitAtBreaks(glExtensionsString);
+		String[] extensions = StringFormat.splitAtBreaks(glExtensionsString);
 		return extensions;
 	}
 }
